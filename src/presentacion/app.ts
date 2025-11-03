ba0460a521da4a32e59fadf719a78549dca2417a
Santiago Herrera Martinez <santiagoh7447@gmail.com>
2025-11-03 15:21:31 -0500

    Creacion de las rutas para medico, adicional se crea el archivo app para inicializar el servidor y codigo para ejecución del mismo en el index

---
+++ src/presentacion/app.ts
@@ -0,0 +1,35 @@
+import Fastify from 'fastify';
+import { configuracion } from '../common/configuracion.js';
+import { medicoRutas } from './rutas/MedicoRutas.js';
+
+
+const app = Fastify({ logger: true });
+
+
+await app.register(medicoRutas, { prefix: '/api' });
+
+
+export const startServer = async (): Promise<void> => {
+  try {
+    await app.listen({
+      port: configuracion.servidor.puerto,
+      host: '0.0.0.0'
+    });
+    app.log.info('El servidor está corriendo...');
+    
+    
+    console.log('📋 Rutas registradas:');
+    app.printRoutes();
+  } catch (err) {
+    app.log.error(`Error al ejecutar el servidor\n ${(err as Error).message}`);
+    
+    const serverError = {
+      code: 'FST_ERR_INIT_SERVER',
+      name: 'ServidorError',
+      statusCode: 500,
+      message: `El servidor no se pudo iniciar: ${(err as Error).message}`,
+    };
+    
+    throw serverError;
+  }
+};
