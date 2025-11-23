4c69786f967d1f0d85f891976e69292590002ade
Santiago Herrera Martinez <santiagoh7447@gmail.com>
2025-11-22 19:40:19 -0500

    pruebas unitarias para las entidades medico y consultorio

--- jest.config.ts
+++ jest.config.ts
@@ -1,32 +1,23 @@
 export default {
-    preset: "ts-jest/presets/default-esm",
-    testEnvironment: "node",
-    roots: ["<rootDir>/tests"],
-    testMatch: ["/*.test.ts"],
-    moduleFileExtensions: ["ts", "js", "json"],
-    collectCoverageFrom: [
-        "src//.ts",
-        "!src/**/.d.ts",
-        "!src//tests/",
-        "!src/index.ts",
-    ],
-    coverageDirectory: "coverage",
-    coverageReporters: ["text", "lcov", "html"],
-    verbose: true,
-    extensionsToTreatAsEsm: [".ts"],
-    moduleNameMapper: {
-        "^(\.{1,2}/.*)\.js$": "$1",
-    },
-    transform: {
-        "^.+\.ts$": [
-            "ts-jest",
-            {
-                useESM: true,
-                tsconfig: {
-                    module: "esnext",
-                    moduleResolution: "bundler",
-                },
-            },
-        ],
-    },
+  preset: "ts-jest/presets/default-esm",
+  testEnvironment: "node",
+
+  roots: ["<rootDir>/tests"],
+
+  testMatch: ["**/*.test.ts"],      // <-- importante
+
+  moduleFileExtensions: ["ts", "js", "json"],
+  
+  transform: {
+    "^.+\\.ts$": [
+      "ts-jest",
+      {
+        useESM: true,
+        tsconfig: {
+          module: "esnext",
+          moduleResolution: "bundler",
+        }
+      }
+    ]
+  }
 };
