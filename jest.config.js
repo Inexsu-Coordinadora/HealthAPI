c34d632e5fd777f6c81ffd71c05e40545b34ffa9
Santiago Herrera Martinez <santiagoh7447@gmail.com>
2025-11-23 13:22:08 -0500

    Se agrega configuracion de coverage para mostrar los porcentajes que se están ejecutando de las pruebas unitarias

--- jest.config.js
+++ jest.config.js
@@ -1,24 +1,32 @@
 export default {
     preset: "ts-jest/presets/default-esm",
     testEnvironment: "node",
+
     roots: ["<rootDir>/tests"],
     testMatch: ["**/*.test.ts"],
+
     moduleFileExtensions: ["ts", "js", "json"],
+
+    collectCoverage: true,
     collectCoverageFrom: [
-        "src//.ts",
-        "!src/**/.d.ts",
-        "!src//tests/",
-        "!src/index.ts",
+        "src/**/*.ts",              
+        "!src/**/*.d.ts",           
+        "!src/**/tests/**",         
+        "!src/index.ts",            
     ],
+
     coverageDirectory: "coverage",
     coverageReporters: ["text", "lcov", "html"],
+
     verbose: true,
     extensionsToTreatAsEsm: [".ts"],
+
     moduleNameMapper: {
-        "^(\.{1,2}/.*)\.js$": "$1",
+        "^(\\.{1,2}/.*)\\.js$": "$1",
     },
+
     transform: {
-        "^.+\.ts$": [
+        "^.+\\.ts$": [
             "ts-jest",
             {
                 useESM: true,
